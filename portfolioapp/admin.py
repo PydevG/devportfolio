012e0fbab6844a21a4e3fefe58bb9591f1617e51
PydevG <peterndindi41@gmail.com>
2025-10-01 17:28:44 +0300

    made some changes

--- portfolioapp/admin.py
+++ portfolioapp/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import GraphicDesign, SoftwareProject
+
+
+@admin.register(GraphicDesign)
+class GraphicDesignAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'client', 'project_date', 'is_featured')
+    list_filter = ('category', 'project_date', 'is_featured')
+    search_fields = ('title', 'description', 'client')
+    list_editable = ('is_featured',)
+    
+@admin.register(SoftwareProject)
+class SoftwareProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'technologies', 'completion_date', 'is_featured')
+    list_filter = ('category', 'completion_date', 'is_featured')
+    search_fields = ('title', 'description', 'technologies')
+    list_editable = ('is_featured',)
