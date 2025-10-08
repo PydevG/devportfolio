6b6106067b3e75a4d183cb8984a6d866da46b4ec
PydevG <peterndindi41@gmail.com>
2025-10-08 15:25:01 +0300

    made major changes

--- portfolioapp/views.py
+++ portfolioapp/views.py
@@ -1,21 +1,33 @@
 from django.shortcuts import render
 from .models import SoftwareProject, GraphicDesign
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-
-# Create your views here.
 def home(request):
-    # Get featured projects first, then others
+    # Get page numbers from request
+    software_page = request.GET.get('software_page', 1)
+    design_page = request.GET.get('design_page', 1)
+    
+    # Software projects pagination
     featured_software = SoftwareProject.objects.filter(is_featured=True).order_by('-completion_date')
     other_software = SoftwareProject.objects.filter(is_featured=False).order_by('-completion_date')
-    software_projects = list(featured_software) + list(other_software)
+    all_software = list(featured_software) + list(other_software)
     
+    software_paginator = Paginator(all_software, 6)  # 6 software projects per page
+    try:
+        software_projects = software_paginator.page(software_page)
+    except (PageNotAnInteger, EmptyPage):
+        software_projects = software_paginator.page(1)
+    
+    # Design projects pagination
     featured_designs = GraphicDesign.objects.filter(is_featured=True).order_by('-project_date')
     other_designs = GraphicDesign.objects.filter(is_featured=False).order_by('-project_date')
-    graphic_designs = list(featured_designs) + list(other_designs)
+    all_designs = list(featured_designs) + list(other_designs)
     
-    # Limit to 12 projects total for better performance
-    software_projects = software_projects[:6]
-    graphic_designs = graphic_designs[:6]
+    design_paginator = Paginator(all_designs, 6)  # 6 design projects per page
+    try:
+        graphic_designs = design_paginator.page(design_page)
+    except (PageNotAnInteger, EmptyPage):
+        graphic_designs = design_paginator.page(1)
     
     context = {
         'software_projects': software_projects,
