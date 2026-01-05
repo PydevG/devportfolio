fc8d9570c7c4f770f7f3767a65f9b6beeb7d8661
PydevG <peterndindi41@gmail.com>
2026-01-05 12:56:52 +0300

    sql error

--- devportfolio/settings.py
+++ devportfolio/settings.py
@@ -61,24 +61,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "devportfolio.wsgi.application"
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-            'connect_timeout': 60,
-            'read_timeout': 60,
-            'write_timeout': 60,
-        },
-        'CONN_MAX_AGE': 60,
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': config('DB_HOST'),
+#         'PORT': config('DB_PORT'),
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#             'charset': 'utf8mb4',
+#             'connect_timeout': 60,
+#             'read_timeout': 60,
+#             'write_timeout': 60,
+#         },
+#         'CONN_MAX_AGE': 60,
+#     }
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
